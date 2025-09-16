ccf4c379b8cbfe1f1faee3f18b2651b6b773db31
jeongyun1206 <67818407+jeongyun1206@users.noreply.github.com>
2025-09-16 12:29:27 +0900

    #2 feat: restemplate 빈 생성 설정

---
+++ src/main/java/com/stockport/server/dto/StockInfoResponse.java
@@ -0,0 +1,29 @@
+package com.stockport.server.dto;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class StockInfoResponse {
+    private String basDt;
+    private String srtnCd;
+    private String isinCd;
+    private String mrktCtg;
+    private String itmsNm;
+    private String crno;
+    private String corpNm;
+
+    @Builder
+    public StockInfoResponse(String basDt, String srtnCd, String isinCd, String mrktCtg, String itmsNm, String crno, String corpNm) {
+        this.basDt = basDt;
+        this.srtnCd = srtnCd;
+        this.isinCd = isinCd;
+        this.mrktCtg = mrktCtg;
+        this.itmsNm = itmsNm;
+        this.crno = crno;
+        this.corpNm = corpNm;
+    }
+}
