9b0ac401abd6226b7310b96d1afc080d42f028fd
jeongyun1206 <67818407+jeongyun1206@users.noreply.github.com>
2025-11-18 23:28:22 +0900

    #41 feat: swagger https 설정

--- src/main/java/com/stockport/server/global/config/SwaggerConfig.java
+++ src/main/java/com/stockport/server/global/config/SwaggerConfig.java
@@ -16,6 +16,6 @@ public class SwaggerConfig {
                         .title("StockPort API 명세서")
                         .description("주식 데이터 조회 및 백테스팅 시스템 API 문서")
                         .version("v1.0.0")
-                        .license(new License().name("Apache 2.0").url("http://springdoc.org")));
+                        .license(new License().name("Apache 2.0").url("https://springdoc.org")));
     }
 }
